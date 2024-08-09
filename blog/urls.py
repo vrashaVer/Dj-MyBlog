6c879d1564ba9c2992d3194850cd84458bef9132
vrashaVer <da.programs.vr@gmail.com>
2024-08-09 12:28:41 +0300

    create Author model
    create Author model
    add author in post model
    add pages about post details and posts by author
    add urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 import blog.views as  blog_views
 
 urlpatterns = [
-    path("",blog_views.post_list,name="post_list")
+    path("",blog_views.post_list,name="post_list"),
+    path("post/<int:post_id>/",blog_views.post_detail,name='post_detail'),
+    path('author/<int:author_id>/',blog_views.posts_by_author,name='posts_by_author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from blog.models import Post
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post,Author
 
 
 def post_list(request):
@@ -12,3 +12,26 @@ def post_list(request):
         template_name="blog/post_list.html",
         context=context
     )
+def post_detail(request,post_id):
+    post = get_object_or_404(Post,id=post_id)
+    context = {
+        "post":post
+    }
+    return render(
+        request, 
+        template_name='blog/post_detail.html',
+        context=context
+        )
+
+def posts_by_author(request,author_id):
+    author = get_object_or_404(Author, id=author_id)
+    posts = Post.objects.filter(author=author)
+    context =  {
+        'author': author, 
+        'posts': posts
+        }
+    return render(
+        request, 
+        template_name='blog/posts_by_author.html', 
+        context=context
+        )
